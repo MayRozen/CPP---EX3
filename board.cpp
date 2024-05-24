4b16e5f270b9bb5597c2a68de3e4e934c414d0cb
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-24 17:32:24 +0300

    changes

--- board.cpp
+++ board.cpp
@@ -2,85 +2,34 @@
     Mail: mayrozen45@gmail.com
 */
 
+#include <iostream>
 #include "board.hpp"
 #include "Tile.cpp"
 using namespace std;
-using namespace ariel;
 
-class Board {
-private:
-    std::vector<Tile> tiles;
-    // Add data structure to manage the layout of tiles
+namespace ariel {
 
-public:
-    Board() {
-        initialize();
+    vector<Tile*> Board::getTiles() const{
+        return tiles;
     }
 
-    void initialize() {
-        // The board is arranged according to the example which in the rules book - from the left to the right.
-        
-
-        // -----------------------------Sea-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------line 1-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        tiles.push_back(Tile(ResourceType::Mountains, 10));
-        tiles.push_back(Tile(ResourceType::Pasture, 2));
-        tiles.push_back(Tile(ResourceType::Forest, 9));
-
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------line 2-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        tiles.push_back(Tile(ResourceType::Fields, 12));
-        tiles.push_back(Tile(ResourceType::Hills, 6));
-        tiles.push_back(Tile(ResourceType::Pasture, 4));
-        tiles.push_back(Tile(ResourceType::Hills, 10));
-
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------line 3-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        tiles.push_back(Tile(ResourceType::Fields, 9));
-        tiles.push_back(Tile(ResourceType::Forest, 11));
-        tiles.push_back(Tile(ResourceType::Desert, 0));
-        tiles.push_back(Tile(ResourceType::Forest, 3));
-        tiles.push_back(Tile(ResourceType::Mountains, 8));
-
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------line 4-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        tiles.push_back(Tile(ResourceType::Forest, 8));
-        tiles.push_back(Tile(ResourceType::Mountains, 3));
-        tiles.push_back(Tile(ResourceType::Fields, 4));
-        tiles.push_back(Tile(ResourceType::Pasture, 5));
-
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------line 5-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        tiles.push_back(Tile(ResourceType::Hills, 5));
-        tiles.push_back(Tile(ResourceType::Fields, 6));
-        tiles.push_back(Tile(ResourceType::Pasture, 11));
-
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-
-        // -----------------------------Sea-----------------------------
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-        tiles.push_back(Tile(ResourceType::Sea, 0));
-        
+    int Board::getBoardIndex() const{
+        return boardIndex;
     }
 
-    // Method to get tile information, place settlements/roads, etc.
-};
+    void Board::printAsMatrix() const{ // Print the board as a matrix
+        for(size_t i=0; i<tiles.size(); i++){
+            if(i==2 || i==7 || i==13 || i==20 || i==26 || i==31 || i==34){
+                cout << tiles[i] << endl;
+            }
+            else{
+                cout << tiles[i] << " ";
+            }
+        }
+    }
+
+    Tile* Board::getTileAt(int boardIndex) const{
+        return tiles[boardIndex];
+    }
+
+}
