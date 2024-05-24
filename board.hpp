281193e4600e242306eeafdb62c74b0e9006305c
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-24 16:03:35 +0300

    starting

--- board.hpp
+++ board.hpp
@@ -1,3 +1,28 @@
 /*  ID: 212051007
     Mail: mayrozen45@gmail.com
 */
+
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+namespace ariel{
+    class Board {
+    private:
+
+
+
+
+
+    public:
+
+
+
+    };
+
+} 
+
+#endif // BOARD_HPP
