525fed5e183489c100d751f5453e27b4c4020dd3
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-26 11:35:46 +0300

    first update

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,43 @@
 /*  ID: 212051007
     Mail: mayrozen45@gmail.com
 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Tile.cpp"
+#include "Structure.cpp"
+#include "player.hpp"
+using namespace std;
+
+namespace ariel{
+
+    void Player::addRoad(string name, vector<Tile> r) {
+        roads.push_back(Structure(name, r));
+    }
+
+    void Player::addSettlement(string name, vector<Tile> r) {
+        settlements.push_back(Structure(name, r));
+    }
+
+    void Player::addCity(string name, vector<Tile> r) {
+        cities.push_back(Structure(name, r));
+    }
+
+    void Player::printPlayerInfo() {
+        cout << "Roads:" << endl;
+        for (const auto& road : roads) {
+            cout << "Name: " << road.name << endl;
+        }
+
+        cout << "Settlements:" << endl;
+        for (const auto& settlement : settlements) {
+            cout << "Name: " << settlement.name << endl;
+        }
+
+        cout << "Cities:" << endl;
+        for (const auto& city : cities) {
+            cout << "Name: " << city.name << endl;
+        }
+    }
+}
