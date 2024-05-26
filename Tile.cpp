adf85570eb994631314fb3d482de0d9255c21b64
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-26 11:38:02 +0300

    add recources type

--- Tile.cpp
+++ Tile.cpp
@@ -1,6 +1,11 @@
 /*  ID: 212051007
     Mail: mayrozen45@gmail.com
 */
+#ifndef TILE_HPP
+#define TILE_HPP
+
+#include <iostream>
+#include <string>
 
 
 enum class ResourceType { Hills, Forest, Mountains, Fields, Pasture, Desert, Sea };
@@ -15,8 +20,52 @@ Desert - produce nothing
 
 class Tile {
 public:
-    ResourceType resource; // The type of the resource
+    ResourceType tileResource; // The type of the resource
     int number;  // The number token on the tile
 
-    Tile(ResourceType resource, int number) : resource(resource), number(number) {}
+    Tile(ResourceType resource, int num) : tileResource(resource), number(num) {}
+
+    void print() const;
 };
+
+class SeaTile : public Tile {
+public:
+    SeaTile() : Tile(ResourceType::Sea, 0) {}
+
+    void print() const {
+        std::cout << "Sea" << std::endl;
+    }
+};
+
+class ResourceTile : public Tile {
+public:
+    enum class ResourceType {Lumber, Brick, Wool, Grain, Ore, Hills, Fields, Forest, Pasture, Mountains, Desert};
+
+    ResourceType resource;
+
+    ResourceTile(ResourceType resource, int number) : Tile(resource, number) {}
+
+    void print() const {
+        std::cout << "Resource: " << getResourceName(resource) << ", Number: " << number << std::endl;
+    }
+
+private:
+    std::string getResourceName(ResourceType resource) const {
+        switch (resource) {
+            case ResourceType::Lumber: return "Lumber";
+            case ResourceType::Brick: return "Brick";
+            case ResourceType::Wool: return "Wool";
+            case ResourceType::Grain: return "Grain";
+            case ResourceType::Ore: return "Ore";
+            case ResourceType::Hills: return "Hills";
+            case ResourceType::Fields: return "Fields";
+            case ResourceType::Forest: return "Forest";
+            case ResourceType::Pasture: return "Pasture";
+            case ResourceType::Mountains: return "Mountains";
+            case ResourceType::Desert: return "Desert";
+            default: return "Unknown";
+        }
+    }
+};
+
+#endif // TILE_HPP
