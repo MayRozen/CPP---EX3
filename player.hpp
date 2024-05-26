554a6177004df4155aacd0c85ec71d85932a8223
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-26 11:36:24 +0300

    first update

--- player.hpp
+++ player.hpp
@@ -1,3 +1,37 @@
 /*  ID: 212051007
     Mail: mayrozen45@gmail.com
 */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Tile.cpp"
+#include "Structure.cpp"
+using namespace std;
+
+namespace ariel{
+
+class Player {
+public:
+    string name;
+    vector<Structure> roads;
+    vector<Structure> settlements;
+    vector<Structure> cities;
+
+    Player(string n) : name(n) {} // constructor
+    ~Player() { // distructor
+        this->name = "";
+        this->roads.clear();
+        this->settlements.clear();
+        this->cities.clear();
+    } 
+
+    void addRoad(string name, vector<Tile> r);
+
+    void addSettlement(string name, vector<Tile> r);
+
+    void addCity(string name, vector<Tile> r);;
+
+    void printPlayerInfo();
+};
+}
