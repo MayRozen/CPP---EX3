b73b6c025d69e3f579c0e5571e89d235b5360672
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-27 23:39:39 +0300

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -33,5 +33,22 @@ public:
     void addCity(string name, vector<Tile> r);;
 
     void printPlayerInfo();
+
+    void placeSettelemnt(places, placesNum, board);
+
+    void placeRoad(places, placesNum, board);
+
+    void rollDice();                                    // Lets say it's print 4. Then, p2 gets ore from the mountations.
+    
+    void placeRoad({"Forest", "Hills"}, {5, 6}, board); // p1 continues to build a road.
+    
+    void trade(p2, "wood", "brick", 1, 1); // p1 trades 1 wood for 1 brick with p2.
+    
+    bool endTurn();                        // p1 ends his turn.
+    
+    void buyDevelopmentCard(); // p2 buys a development card. Lets say it is a bonus points card.
+    
+    void printPoints(); // p1 has 2 points because it has two settelments.
+    
 };
 }
