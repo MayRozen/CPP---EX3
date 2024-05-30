e1c26d255b462cd84f42d22c3257e1baab788a39
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-05-30 15:19:27 +0300

    finish

--- Structure.cpp
+++ Structure.cpp
@@ -9,11 +9,31 @@
 using namespace std;
 
 namespace ariel{
-class Structure {
+class structure { // Abstruct class
 public:
     string name;
-    vector<Tile> resources;
+    vector<ReturnRes> resources; // The resources we need for build the structure
 
-    Structure(string n, vector<Tile> r) : name(n), resources(r) {}
+
+    structure(string n, vector<ReturnRes> r) : name(n), resources(r) {}
+
+    virtual ~structure();
+    
 };
+
+class road : public structure { // Requires: Brick & Lumber
+public:
+    road() : structure("Road", {ReturnRes::Brick, ReturnRes::Lumber}) {}
+};
+
+class settlement : public structure { // Requires: Brick, Lumber, Wool, & Grain
+public:
+    settlement() : structure("Settlement", {ReturnRes::Brick, ReturnRes::Lumber, ReturnRes::Wool, ReturnRes::Grain}) {}
+};
+
+class city : public structure { // Requires: 3 Ore & 2 Grain
+public:
+    city() : structure("City", {ReturnRes::Ore, ReturnRes::Ore, ReturnRes::Ore, ReturnRes::Grain, ReturnRes::Grain}) {}
+};
+
 }
