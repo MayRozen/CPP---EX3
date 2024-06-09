f89543d78fc67494f310dc81fd760902236e2c1b
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-06-09 21:14:16 +0300

    end developing card

--- catan.cpp
+++ catan.cpp
@@ -2,28 +2,80 @@
     Mail: mayrozen45@gmail.com
 */
 
+#ifndef CATAN_HPP
+#define CATAN_HPP
+
 #include <iostream>
 #include <vector>
-#include "catan.hpp"
 #include "player.hpp"
 #include "board.hpp"
 using namespace std;
 
 namespace ariel{
 
-    Board Catan::getBoard(){ // get the board of the game
-        return *this->gameBoard;
-    }
+    class Catan {
+    private:
+        static Catan* instance; // catan need to be as a singelton
+        vector<Player> players;
+        Board *gameBoard;
+        Player *winner; 
 
-    vector<Player>& Catan::getPlayers(){
-        return this->players;
-    }
+         // Private constructor to prevent instantiation
+        Catan(const Player& p1, const Player& p2, const Player& p3) {
+            players.push_back(p1);
+            players.push_back(p2);
+            players.push_back(p3);
+            gameBoard = new Board(); // Assume Board has a default constructor
+            winner = nullptr; // There is no winner yet
+        }
 
-    Player Catan::ChooseStartingPlayer(){ // should print the name of the starting player
+        ~Catan(){ // distructor
+            this->players.clear();
+            delete this->gameBoard;
+            delete this->winner;
+        }
+        
+    public:
 
-    }
+        // void initializePlayers(Player p1, Player p2, Player p3) {
+        //     if (players.size() == 3) {
+        //         cout << "Game already initialized with 3 players." << endl;
+        //         return;
+        //     }
 
-    void Catan::printWinner(){ // Should print None because no player reached 10 points.
-        cout << " The winner is:" << winner << endl;
-    }
+        //     players.push_back(p1);
+        //     players.push_back(p2);
+        //     players.push_back(p3);
+
+        //     cout << "Game initialized with 3 players." << endl;
+        // }
+
+        static Catan* getInstance(const Player& p1, const Player& p2, const Player& p3) {
+            if (!instance) {
+                instance = new Catan(p1, p2, p3);
+            }
+            return instance;
+        }
+        
+        // Public method to get the singleton instance without arguments
+        static Catan* getInstance() {
+            return instance;
+        }
+
+
+        void printPlayers() const {
+            for (const auto& player : players) {
+                cout << player.name << endl;
+            }
+        }
+
+        vector<Player>& getPlayers(); // Return this->Players
+        Board getBoard();  // get the board of the game
+        Player ChooseStartingPlayer();   // should print the name of the starting player
+        void printWinner(); // Should print None because no player reached 10 points.
+    };
+
+    Catan* instance = nullptr;
 }
+
+#endif // CATAN_HPP
