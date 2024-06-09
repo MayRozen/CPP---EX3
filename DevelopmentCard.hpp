41eb948319d242146e8506b31f5fbb968662f9a5
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-06-09 18:43:20 +0300

    Update DevelopmentCard.hpp

--- DevelopmentCard.hpp
+++ DevelopmentCard.hpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include "catan.hpp"
+#include "Tile.cpp"
 using namespace std;
 
 namespace ariel {
@@ -33,50 +34,30 @@ public:
     virtual ~DevelopmentCard() {}
     virtual CardType getType() const = 0;
     virtual string getCardName() const = 0;
-    virtual void useCard() = 0;
+    virtual void useCard(Player& p) = 0;
 };
 
 class MonopolyCard : public DevelopmentCard {
 public:
     CardType getType() const override { return CardType::Monopoly; }
+
     string getCardName() const override { return "Monopoly"; }
-    void useCard(Player& p) override {
-        Catan* game = Catan::getInstance();
-        vector<Player>& players = game->getPlayers();
-        ResourceType takenRes; // The resource the player want to take
 
-        int sumTakenRes = 0;
-
-        for (size_t i = 0; i < players.size(); i++) {
-            Player& player = players[i];
-            for (size_t j = 0; j < player.returnRes.size(); j++) {
-                while (player.returnRes[j] == takenRes) {
-                    sumTakenRes++;
-                    player.returnRes.erase(player.returnRes.begin() + j); // Remove the resource from the player's list
-                }
-            }
-        }
-
-        for(size_t i=0;  i<(size_t)sumTakenRes; i++){
-            p.returnRes
-        }
-    }
+    void useCard(Player& p) override;
 };
 
 class BuildingRoadsCard : public DevelopmentCard {
 public:
     CardType getType() const override { return CardType::BuildingRoads; }
     string getCardName() const override { return "Building Roads"; }
-    void useCard() override {
-        cout << "Building Roads: Build 2 roads without any resource payment." << endl;
-    }
+    void useCard(Player& p) override;
 };
 
 class YearOfAbundanceCard : public DevelopmentCard {
 public:
     CardType getType() const override { return CardType::YearOfAbundance; }
     string getCardName() const override { return "Year Of Abundance"; }
-    void useCard() override {
+    void useCard(Player& p) override {
         cout << "Year Of Abundance: Choose 2 resources without any resource payment." << endl;
     }
 };
@@ -85,7 +66,7 @@ class KnightCard : public DevelopmentCard {
 public:
     CardType getType() const override { return CardType::Knight; }
     string getCardName() const override { return "Knight"; }
-    void useCard() override {
+    void useCard(Player& p) override {
         cout << "Knight: Move the robber." << endl;
     }
 };
@@ -94,7 +75,7 @@ class VictoryPointCard : public DevelopmentCard {
 public:
     CardType getType() const override { return CardType::VictoryPoint; }
     string getCardName() const override { return "Victory Point"; }
-    void useCard() override {
+    void useCard(Player& p) override {
         cout << "Victory Point: Gain 1 victory point." << endl;
     }
 };
