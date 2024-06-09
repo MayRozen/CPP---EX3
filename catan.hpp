76a192ade3fa82ed481a7ecc4aac15051ded0570
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-06-09 20:20:22 +0300

    Update catan.hpp

--- catan.hpp
+++ catan.hpp
@@ -20,11 +20,15 @@ namespace ariel{
         Board *gameBoard;
         Player *winner; 
 
-        Catan(){  // constructor
-            this->players;
-            this->gameBoard;
-            this->winner; // THere is no winner yet  
+         // Private constructor to prevent instantiation
+        Catan(const Player& p1, const Player& p2, const Player& p3) {
+            players.push_back(p1);
+            players.push_back(p2);
+            players.push_back(p3);
+            gameBoard = new Board(); // Assume Board has a default constructor
+            winner = nullptr; // There is no winner yet
         }
+
         ~Catan(){ // distructor
             this->players.clear();
             delete this->gameBoard;
@@ -33,11 +37,30 @@ namespace ariel{
         
     public:
 
-        static Catan* getInstance(){
-            if(!instance){
-                instance = new Catan();
+        // void initializePlayers(Player p1, Player p2, Player p3) {
+        //     if (players.size() == 3) {
+        //         cout << "Game already initialized with 3 players." << endl;
+        //         return;
+        //     }
+
+        //     players.push_back(p1);
+        //     players.push_back(p2);
+        //     players.push_back(p3);
+
+        //     cout << "Game initialized with 3 players." << endl;
+        // }
+
+        static Catan* getInstance(const Player& p1, const Player& p2, const Player& p3) {
+            if (!instance) {
+                instance = new Catan(p1, p2, p3);
             }
             return instance;
+        }
+
+        void printPlayers() const {
+            for (const auto& player : players) {
+                cout << player.name << endl;
+            }
         }
 
         vector<Player>& getPlayers(); // Return this->Players
